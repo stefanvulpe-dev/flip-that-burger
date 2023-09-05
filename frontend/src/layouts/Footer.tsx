ada7ba2c918fff21f9a9b384813de77723ba706d
Stefan Vulpe <svulpe25@gmail.com>
2023-09-05 18:09:20 +0300

    new dist

--- frontend/src/layouts/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { FooterContainer } from './containers/FooterContainer';
-
-export function Footer() {
-  return (
-    <footer className='mt-16 bg-neutral-300'>
-      <FooterContainer />
-    </footer>
-  );
-}
--- frontend/src/layouts/Hero.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroCarousel } from '../components/index';
-
-export function Hero() {
-  return (
-    <div className='z-10 bg-accent-100 py-8 md:py-12 min-h-screen flex flex-col gap-6 justify-center items-center'>
-      <HeroCarousel />
-    </div>
-  );
-}
