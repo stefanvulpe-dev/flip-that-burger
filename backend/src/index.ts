744f1b8b5d1d6d0c6513da21f15ee35ef846b1fc
Stefan Vulpe <svulpe25@gmail.com>
2023-09-15 16:45:23 +0300

    chore: add configuration files and dependencies

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(5000, () => {
+  console.log('Server started on http://localhost:5000');
+});
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection.on('error', (err: Error) => {
+  console.error(err);
+  console.log('MongoDB connection error. Please make sure MongoDB is running.');
+  process.exit();
+});
